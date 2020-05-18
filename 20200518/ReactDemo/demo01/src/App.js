78a77a35eabcf469e52354be3e2d2d4ee84bcd2d
ZeRockmanX <ZeRockmanX@gmail.com>
2020-05-18 21:04:46 +0800

    add test

--- 20200518/ReactDemo/demo01/src/App.js
+++ 20200518/ReactDemo/demo01/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+//Es6写法的结构赋值 等同于
+//import React from 'react'
+//const Component = React.Component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// component允许使用js写html的语法（语法稍微有点区别）
+class App extends Component {
+    render() {
+        return (
+            // jsx写法 简略含义：在代码里 <>中是html语法，{}是js语法
+            <div className='title'>
+                {true ? '111' : '222'}
+                Hello World
+            </div>
+        )
+        // js写法
+        // var child1 = React.createElement('li',null,'Hello world for js')
+        // var root = React.createElement('ul',{className:'title'},child1)
+    }
 }
 
-export default App;
+//给组建起名字，暴露组建接口 自定义组建必须大写开头
+export default App
