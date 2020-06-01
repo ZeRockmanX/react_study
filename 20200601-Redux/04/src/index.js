940c8aa88ad3bed98314932ccb369684771363e8
ZeRockmanX <ZeRockmanX@gmail.com>
2020-06-02 00:53:34 +0800

    study 4

---
+++ 20200601-Redux/04/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tx01 from './Tx01';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <>
+      <Tx01 />
+    </>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
--- 20200601-Redux/readme.md
+++ 20200601-Redux/readme.md
@@ -22,3 +22,4 @@ const store = createStore(
 01 初始化以及读取store中的数据
 02 更新store中的数据 state数据从redux获取新的数据并展示
 03 点击功能 input中的数据放到store中的list里面，再将store中更新后的list数据返还给state（setState渲染页面）
+04 list中点击删除项目事件
